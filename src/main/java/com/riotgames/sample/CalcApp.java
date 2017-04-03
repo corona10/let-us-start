d40d01da235e7edbc2adf5358bb48f7ceec916fd
Janghun Jo <everestbeta524@gmail.com>
2017-04-03 14:09:43 +0900

    Fixed String concatenation.

--- src/main/java/com/riotgames/sample/CalcApp.java
+++ src/main/java/com/riotgames/sample/CalcApp.java
@@ -104,7 +104,7 @@ public class CalcApp {
     public static void main(String[] args) {
         final CalcApp app = new CalcApp();
         final StringBuilder outputs = new StringBuilder();
-        Arrays.asList(args).forEach(value -> outputs.append(value + " "));
+        Arrays.asList(args).forEach(value -> outputs.append(value).append(" "));
         System.out.print("Addition of values: " + outputs + " = ");
         System.out.println(app.calc(args));
     }
